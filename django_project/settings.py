b474ce9f80d3fc03309b00e5001344737d769d20
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2021-05-17 21:36:15 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -148,8 +148,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sggajare360@gmail.com'
-EMAIL_HOST_PASSWORD = 'googkalpesh@11'
+
 
 
 ACCOUNT_EMAIL_REQUIRED = True
